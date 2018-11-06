56141d0318663623ae776ff627ddd0f96c3c8b6e
CircuitSyn <circuitsyn@gmail.com>
2018-11-05 16:01:04 -0800

    server working

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-var path = require("path");
+// var path = require("path");
 
 // Sets up the Express App
 // =============================================================
@@ -32,8 +32,8 @@ app.use(express.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
 // Starts the server to begin listening
